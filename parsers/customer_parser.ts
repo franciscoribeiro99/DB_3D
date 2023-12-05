9f17410ccd8650aa8cad8e3e596239a322097a75
MartTave <martin.tavernier256@gmail.com>
2023-12-05 11:14:27 +0100

    Made better parser

--- parsers/customer_parser.ts
+++ parsers/customer_parser.ts
@@ -1,29 +1,7 @@
-import fs from 'fs';
-import { parse } from 'csv-parse';
+import { getCSV } from "../tools/CSVParser";
+async function main() {
+    const res = await getCSV("./data/customer_data.csv", ";")
+    console.log(res)
+}
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { Type } from 'typescript';
-
-const baseFolder = "./data/";
-
-
-const processFile = async () => {
-  const records = [];
-  const parser = fs
-    .createReadStream(baseFolder + "customer_data.csv")
-    .pipe(parse({
-        delimiter: ";"
-    // CSV options if any
-    }));
-  for await (const record of parser) {
-    // Work with each record
-    records.push(record);
-  }
-  return records;
-};
-
-(async () => {
-  const records = await processFile();
-  console.log(records);
-})();
+main();
---
+++ tools/CSVParser.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
+export async function getCSV(filename: string, delimiter:string = ",") {
+    return new Promise(async (resolve, reject) => {
+        const records = [];
+        const parser = fs.createReadStream(filename).pipe(parse({
+            delimiter: delimiter
+            // CSV options if any
+        }));
+        for await (const record of parser) {
+            // Work with each record
+            records.push(record);
+        }
+        resolve(records);
+    });
+}
